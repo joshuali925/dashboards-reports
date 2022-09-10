8c49af3c722cce1ca97a0cd83188e775a630c11b
Joshua Li <joshuali925@gmail.com>
2022-09-09 18:50:06 -0700

    Update build_headless_chrome.sh

--- dashboards-reports/rendering-engine/headless-chrome/build_headless_chrome.sh
+++ dashboards-reports/rendering-engine/headless-chrome/build_headless_chrome.sh
@@ -129,7 +129,20 @@ if [[ "$platform_name" = "linux" ]]; then
   if [[ "$arch_name" = "arm64" ]]; then
     ./build/linux/sysroot_scripts/install-sysroot.py --arch=$arch_name
   fi
-  ./build/install-build-deps.sh
+  if builtin command -v yum > /dev/null 2>&1; then
+    sudo yum install git python bzip2 tar pkgconfig atk-devel alsa-lib-devel \
+      bison binutils brlapi-devel bluez-libs-devel bzip2-devel cairo-devel \
+      cups-devel dbus-devel dbus-glib-devel expat-devel fontconfig-devel \
+      freetype-devel gcc-c++ glib2-devel glibc.i686 gperf glib2-devel \
+      gtk3-devel java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel \
+      libgcc.i686 libgnome-keyring-devel libjpeg-devel libstdc++.i686 libX11-devel \
+      libXScrnSaver-devel libXtst-devel libxkbcommon-x11-devel ncurses-compat-libs \
+      nspr-devel nss-devel pam-devel pango-devel pciutils-devel \
+      pulseaudio-libs-devel zlib.i686 httpd mod_ssl php php-cli python-psutil wdiff \
+      xorg-x11-server-Xvfb
+  else
+    ./build/install-build-deps.sh
+  fi
 fi
 
 
